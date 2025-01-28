70fa0d211adbf4c533b43d6c2b3ae5bd34ef6c13
ImmortalAccord <qwertyuiop1238080@gmail.com>
2025-01-28 14:20:27 +0300

    28.01.25

---
+++ dz/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private List<IProduct> _products = new List<IProduct>();
+        private void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                var newProduct = new Product();
+                newProduct.Name = Tovar.Text;
+                if (decimal.TryParse(Price.Text, out decimal price))
+                {
+                    newProduct.Price = price;
+                    _products.Add(newProduct);
+                    List_Tovars.ItemsSource = null;
+                    List_Tovars.ItemsSource = _products;
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный ввод цены");
+                }
+            }
+        }
+    }
+}
---
+++ dz/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz
+{
+    public class Product : IProduct
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        }
+    }
+}
